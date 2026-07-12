237b0d934fc190535be20a92a48e3c707eca5903
bharathasicdv-png <bharath.asic.dv@gmail.com>
2026-07-12 15:11:37 +0530

    Define transaction fields in pcie_transaction class
    Added fields for address, data, write, and read to the PCIe transaction class.

--- tb/seq_item/pcie_transaction.sv
+++ tb/seq_item/pcie_transaction.sv
@@ -1,3 +1,26 @@
+//------------------------------------------------------------
+// File : pcie_transaction.sv
+// Description : PCIe Transaction Class
+//------------------------------------------------------------
+
 class pcie_transaction extends uvm_sequence_item;
+
+    // Transaction Fields
+
+    rand bit [31:0] address;
+    rand bit [31:0] data;
+
+    rand bit        write;
+    rand bit        read;
+
+    // Constructor
+
+    function new(string name = "pcie_transaction");
+        super.new(name);
+    endfunction
+
+    // Register with Factory
+
+    `uvm_object_utils(pcie_transaction)
 
 endclass
