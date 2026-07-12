ec3948e2045ba74531a8b78b07b097aee55428f1
bharathasicdv-png <bharath.asic.dv@gmail.com>
2026-07-12 19:03:54 +0530

    Create PCIe verification interface in pcie_if.sv
    Define PCIe verification interface with reset and TX/RX channels.

--- tb/interface/pcie_if.sv
+++ tb/interface/pcie_if.sv
@@ -1,1 +1,38 @@
 
+//--------------------------------------------------------------
+// Project : PCIe Verification Project
+// File    : pcie_if.sv
+// Author  : Bharath
+// Description : PCIe Verification Interface
+//--------------------------------------------------------------
+
+`ifndef PCIE_IF_SV
+`define PCIE_IF_SV
+
+interface pcie_if(input logic clk);
+
+    //----------------------------------------------------------
+    // Reset
+    //----------------------------------------------------------
+
+    logic rst_n;
+
+    //----------------------------------------------------------
+    // TX Channel
+    //----------------------------------------------------------
+
+    logic        tx_valid;
+    logic [31:0] tx_data;
+    logic        tx_ready;
+
+    //----------------------------------------------------------
+    // RX Channel
+    //----------------------------------------------------------
+
+    logic        rx_valid;
+    logic [31:0] rx_data;
+    logic        rx_ready;
+
+endinterface
+
+`endif
